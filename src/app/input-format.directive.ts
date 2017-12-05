590cd734c76b02f332baa43556840fbb7e87da7c
Oz <lisiy50@gmail.com>
2017-12-05 22:34:35 +0200

    fff

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
+
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
-
-  constructor() { }
-
+  @Input('appInputFormat') format;
+  constructor(private el:ElementRef) {
+    
+  }
+  // @HostListener('focus') onfocus() {
+  //   let value: string = this.el.nativeElement.value;
+  //   this.el.nativeElement.value = value.toUpperCase();
+  // }
+  @HostListener('blur') onBlur() {
+    let value:string = this.el.nativeElement.value;
+    if (this.format == 'lowercase') {
+      this.el.nativeElement.value = value.toLowerCase();
+    } else {
+      this.el.nativeElement.value = value.toUpperCase();
+    }
+  }
 }
---
+++ src/app/zippy/zippy.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-zippy',
+  templateUrl: './zippy.component.html',
+  styleUrls: ['./zippy.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ZippyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
